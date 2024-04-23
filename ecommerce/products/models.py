10849a078ecba70691c917869d307eace7d15528
AlfredoB14 <alfredobarranco14@gmail.com>
2024-04-22 18:44:03 -0600

    Product added

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -8,4 +8,16 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
-    category = models.ForeignKey('Category', )
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name #DEBE SER STRING
+    
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name 
